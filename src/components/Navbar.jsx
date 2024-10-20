5c659f209d50529ea828e3902c985b3b1499fd6c
Muskan Thakur <muskanthakur2064@gmail.com>
2024-10-20 14:37:03 +0000

    corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,41 +2,31 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { IoGlobeOutline } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
-// import { LuMenu } from "react-icons/lu";
 
 export default function Navbar() {
+    let navData = [
+        { path: "/", name: "Gallery" },
+        { path: "/blog", name: "Blog" },
+        { path: "/about", name: "About" },
+        { path: "/contact", name: "Contact" },
+    ]
     return (
         <div className="h-screen w-1/2 flex flex-col relative">
             <div className="absolute top-[40%] left-1/4">
                 <div className="fixed">
                     <h1 className="text-3xl font-light text-zinc-900 tracking-tight text-center uppercase">Travel with <br /> <span className="font-semibold  text-black text-5xl font-anton ">Catherine</span></h1>
                     <div className="flex items-center justify-center flex-col mt-5 gap-5">
-                        <NavLink
-                            to="/"
-                            className="text-xl hover:text-blue-600 font-regular tracking-tight text-zinc-500 ">Gallery</NavLink>
-
-                        <NavLink style={(e) => {
-                            return {
-                                color: e.isActive ? "rgb(37 99 235)" : ""
-                            }
-                        }}
-                            to="/blog"
-                            className="text-xl hover:text-blue-600 font-regular tracking-tight text-zinc-500 ">Blog</NavLink>
-                        <NavLink style={(e) => {
-                            return {
-                                color: e.isActive ? "rgb(37 99 235)" : ""
-                            }
-                        }}
-                            to="/about"
-                            className="text-xl hover:text-blue-600 font-regular tracking-tight text-zinc-500 ">About</NavLink>
-
-                        <NavLink style={(e) => {
-                            return {
-                                color: e.isActive ? "rgb(37 99 235)" : ""
-                            }
-                        }}
-                            to="/contact"
-                            className="text-xl hover:text-blue-600 font-regular tracking-tight text-zinc-500 ">Contact</NavLink>
+                        {navData.map((nav, idx) => {
+                            return <NavLink
+                                style={(e) => {
+                                    return {
+                                        color: e.isActive ? "rgb(37 99 235)" : ""
+                                    }
+                                }}
+                                to={nav.path}
+                                key={idx}
+                                className="text-xl hover:text-blue-600 font-regular tracking-tight text-zinc-500 ">{nav.name}</NavLink>
+                        })}
                     </div>
                     <div className="flex gap-4 items-center justify-center text-xl mt-3">
                         <a href="#"><FaGithub /></a>
@@ -48,7 +38,6 @@ export default function Navbar() {
                     </div>
                 </div>
             </div>
-            {/* <LuMenu className="md:hidden w-8 h-8 ml-2 mt-2 fixed" /> */}
         </div>
     )
 }
